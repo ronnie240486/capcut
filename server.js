a920fba6d62b5df0f2772f3184cb8a23c08e45a2
ronnie240486 <ronnie240486@gmail.com>
2025-12-18 23:44:50 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1125,4 +1125,4 @@ app.post('/api/process/generate-music', uploadAny, async (req, res) => {
     }
 });
 
-app.listen(PORT, () => { console.log(`Servidor a e
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
