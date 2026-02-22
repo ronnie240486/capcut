57eb8a5298e551166ac40f79752e1271516dee20
ronnie240486 <ronnie240486@gmail.com>
2026-02-22 16:46:27 -0300

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 // Improved CORS
 app.use(cors({
