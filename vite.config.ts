fb9c6b045a7adee31a54e9e7b131cf74c4945fe7
ronnie240486 <ronnie240486@gmail.com>
2026-02-22 16:52:46 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       server: {
-        port: 3000,
+        port: 8080,
         host: '0.0.0.0',
         allowedHosts: ['capcut-production.up.railway.app']
       },
