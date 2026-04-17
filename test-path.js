4895badf87bd98ff18441be49c5436599a2bab3d
ronnie240486 <ronnie240486@gmail.com>
2026-04-16 22:57:21 -0300

    Update test-path.js

--- test-path.js
+++ test-path.js
@@ -3,8 +3,9 @@ import path from 'path';
 
 console.log('CWD:', process.cwd());
 console.log('__dirname (simulated):', path.dirname(new URL(import.meta.url).pathname));
-console.log('Exists video_engine:', fs.existsSync('./video_engine'));
-console.log('Exists video_engine/exportVideo.js:', fs.existsSync('./video_engine/exportVideo.js'));
-if (fs.existsSync('./video_engine')) {
-    console.log('Contents of video_engine:', fs.readdirSync('./video_engine'));
+console.log('Exists videoengine:', fs.existsSync('./videoengine'));
+console.log('Exists videoengine/exportVideo.js:', fs.existsSync('./videoengine/exportVideo.js'));
+if (fs.existsSync('./videoengine')) {
+    console.log('Contents of videoengine:', fs.readdirSync('./videoengine'));
 }
+
